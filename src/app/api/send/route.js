9b4858df968df924fc79d444e76610be654cef0b
Willians Ferreira <williansdesigner@hotmail.com>
2023-11-21 08:13:25 -0300

    uploading project

--- src/app/api/send/route.js
+++ src/app/api/send/route.js
@@ -1,8 +1,8 @@
 // import { EmailTemplate } from '../../../components/EmailTemplate';
 import { NextResponse } from "next/server";
-import { Resend } from "resend";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
 
 export async function POST(req, res) {
